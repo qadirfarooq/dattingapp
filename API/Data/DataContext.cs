c52f994154f6dc3b1d27cea4d210da58fe5cc523
qadirfarooq <farooq.qadir@gmail.com>
2020-12-29 21:24:55 -0600

    section 3 done

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,8 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 namespace API.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -11,23 +11,26 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
+using API.Data;
+using Microsoft.EntityFrameworkCore;
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options =>{
+                options.UseSqlite(_config.GetConnectionString("DefaultDb"));
+            });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -47,7 +50,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
